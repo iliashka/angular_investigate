4c1e28800682b0558ef0e6bd5422f3b9cb2a2fa4
iliashka <69167918+iliashka@users.noreply.github.com>
2021-08-21 18:13:43 +0300

    nothing

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  templateUrl: 'post.component.html',
+  selector: 'app-post',
+  styleUrls: ['post.component.scss']
+})
+export class PostComponent {}
